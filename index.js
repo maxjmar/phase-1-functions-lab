a0b80856549bb7b777c14316a3d3bcdaed23157c
maxjmar <86731281+maxjmar@users.noreply.github.com>
2022-01-12 22:05:53 -0500

    commit tree 17d53bc76033de63c7a0d0b2d9fe7450db9b2e27

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solution in this file!
+// function distanceFromHqInBlocks(someValue) {
+//     let blocksAway
+//     if (someValue >= 42) {
+//         blocksAway = someValue - 42;
+//     }
+//     else if (someValue <= 42) {
+//       blocksAway = 42 - someValue;
+//     }
+//     return blocksAway
+// }
+function distanceFromHqInBlocks(someValue) {
+    let scuberHq = 42;
+    let blocksAway;
+    if (someValue >= scuberHq) {
+        blocksAway = someValue -= scuberHq;
+    }
+    else if (someValue <= scuberHq) {
+      blocksAway = scuberHq -= someValue;
+    }
+    return blocksAway
+}
+
+function distanceFromHqInFeet(someValue) {
+    let dtBlocks = distanceFromHqInBlocks(someValue);
+    let blocksFeet = dtBlocks *= 264;
+    return blocksFeet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let convFeet;
+    if (start >= destination) {
+        convFeet = (start -= destination) * 264;
+    }
+    else if (start <= destination) {
+        convFeet = (destination -= start) * 264;
+    }
+    return convFeet
+}
+
+function calculatesFarePrice(start, destination) {
+    let fareLength = distanceTravelledInFeet(start, destination);
+    let farePrice;
+    if (fareLength <= 400) {
+        farePrice = 0;
+    }
+    else if (fareLength <= 2000) {
+        farePrice = (fareLength -= 400)  * 0.02;
+    }
+    else if (fareLength <= 2499) {
+        farePrice = 25;
+    }
+    else if (fareLength > 2500) {
+        farePrice = 'cannot travel that far';
+    }
+    return farePrice
+}
